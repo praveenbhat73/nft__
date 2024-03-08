24b70ac22e7ac9dc23793e0c18eee6dbae9e5df0
Praveen Narayan Bhat <108626052+praveenbhat73@users.noreply.github.com>
2024-03-08 23:55:56 +0530

    Update NFTContext.js

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -115,7 +115,7 @@ export const NFTProvider = ({ children }) => {
 
   const fetchNFTs = async () => {
     setIsLoadingNFT(false);
-    const provider = new ethers.providers.JsonRpcProvider();
+    const provider = new ethers.providers.JsonRpcProvider('https://eth-goerli.g.alchemy.com/v2/Wh7txfAjK8Dr7o1dUQX-2Wd3herOaXKL');
     const contract = fetchContract(provider);
 
     const data = await contract.fetchMarketItems();
