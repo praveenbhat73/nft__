8a05114cf23fc61a5f2f0439945ccab18aa683a8
praveenbhat73 <bhatpraveen721@gmail.com>
2024-03-08 23:48:47 +0530

    error

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 // const fs = require('fs');
 require('@nomiclabs/hardhat-waffle');
 
-const privateKey = '';
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
@@ -11,7 +10,7 @@ const privateKey = '';
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = fs.readFileSync('.secret').toString().trim();
+
 //df2fpeuqfyf7wkpp
 module.exports = {
   networks: {
